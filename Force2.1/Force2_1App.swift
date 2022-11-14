9154564e5d87f61bf6ac4539e0a1e921b0022627
Joseph W. DeWeese <52636579+rangerboy901@users.noreply.github.com>
2022-11-14 12:25:39 -0600

    Initial Commit

---
+++ Force2.1/Force2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Force2_1App.swift
+//  Force2.1
+//
+//  Created by Joseph Wil;liam DeWeese on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Force2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
